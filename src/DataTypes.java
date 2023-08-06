c205e82a8d20f98a558324c590c3480e2915c608
codekundan <codekundannegi@gmail.com>
2023-08-06 15:31:08 +0530

    Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static int dataTypes(String type) {
+        int size = 0;
+        switch(type) {
+            case "Integer" :
+                size = 4;
+                break;
+            case "Long" :
+                size = 8;
+                break;
+            case "Float" :
+                size = 4;
+                break;
+            case "Double" :
+                size = 8;
+                break;
+            case "Character" :
+                size = 1;
+                break;
+        }
+        return size;
+    }
+}
