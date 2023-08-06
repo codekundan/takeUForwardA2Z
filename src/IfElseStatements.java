a89242d781e4bcf6df2f33d23ac9502e485ff54a
codekundan <codekundannegi@gmail.com>
2023-08-06 15:39:13 +0530

    If Else Statements

---
+++ src/IfElseStatements.java
@@ -0,0 +1,11 @@
+public class IfElseStatements {
+    public static String compareIfElse(int a, int b) {
+        if (a < b) {
+            return "smaller";
+        } else if (a > b) {
+            return "greater";
+        } else {
+            return "equal";
+        }
+    }
+}
